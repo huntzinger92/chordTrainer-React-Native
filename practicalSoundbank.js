c2e81207f86562569f965ba98596b42db0109053
Trevor T. Smith <tts2p4@gmail.com>
2020-05-25 00:17:00 -0500

    Finished recording the funk section. Tentative testing for typos - make sure to do more thoroughly.

--- practicalSoundbank.js
+++ practicalSoundbank.js
@@ -104,8 +104,132 @@ export const practicalSoundbank = {
   ],
   'funk': [
     {
-      src: require('./assets/chords/drumLoopTest.mp3'),
-      correctChords: ['im7', 'bIIM7', 'IIIM7', 'bIIM7'],
+      src: require('./assets/chords/funk1b21b2.mp3'),
+      correctChords: ['im7', 'bIIM7', 'im7', 'bIIM7'],
+    },
+    {
+      src: require('./assets/chords/funk1b215alt.mp3'),
+      correctChords: ['im7', 'bIIM7', 'im7', 'V7alt'],
+    },
+    {
+      src: require('./assets/chords/funk145b2.mp3'),
+      correctChords: ['im7', 'ivm7', 'V7', 'bIIM7'],
+    },
+    {
+      src: require('./assets/chords/funk145altb2.mp3'),
+      correctChords: ['im7', 'ivm7', 'V7alt', 'bIIM7'],
+    },
+    {
+      src: require('./assets/chords/funk13b21.mp3'),
+      correctChords: ['im7', 'IIIM7', 'bIIM7', 'im7'],
+    },
+    {
+      src: require('./assets/chords/funk1251.mp3'),
+      correctChords: ['im7', 'iiø7', 'V7', 'im7'],
+    },
+    {
+      src: require('./assets/chords/funk125alt1.mp3'),
+      correctChords: ['im7', 'iiø7', 'V7alt', 'im7'],
+    },
+    {
+      src: require('./assets/chords/funk1254.mp3'),
+      correctChords: ['im7', 'iiø7', 'V7', 'ivm7'],
+    },
+    {
+      src: require('./assets/chords/funk125alt6.mp3'),
+      correctChords: ['im7', 'iiø7', 'V7alt', 'VIM7'],
+    },
+    {
+      src: require('./assets/chords/funk136b2.mp3'),
+      correctChords: ['im7', 'IIIM7', 'VIM7', 'bIIM7'],
+    },
+    {
+      src: require('./assets/chords/funk1415.mp3'),
+      correctChords: ['im7', 'ivm7', 'im7', 'V7'],
+    },
+    {
+      src: require('./assets/chords/funk1473.mp3'),
+      correctChords: ['im7', 'ivm7', 'VII7', 'IIIM7'],
+    },
+    {
+      src: require('./assets/chords/funk1435alt.mp3'),
+      correctChords: ['im7', 'ivm7', 'IIIM7', 'V7alt'],
+    },
+    {
+      src: require('./assets/chords/funk17dim76dim.mp3'),
+      correctChords: ['im7', 'vii°7', 'VII7', 'viø7'],
+    },
+    {
+      src: require('./assets/chords/funk17dim1b2.mp3'),
+      correctChords: ['im7', 'vii°7', 'im7', 'bIIM7'],
+    },
+    {
+      src: require('./assets/chords/funk14dom14dom.mp3'),
+      correctChords: ['im7', 'IV7', 'im7', 'IV7'],
+    },
+    {
+      src: require('./assets/chords/funk14dom66.mp3'),
+      correctChords: ['im7', 'IV7', 'VIM7', 'VIM7'],
+    },
+    {
+      src: require('./assets/chords/funk14dom65alt.mp3'),
+      correctChords: ['im7', 'IV7', 'VI7', 'V7alt'],
+    },
+    {
+      src: require('./assets/chords/funk14dom45.mp3'),
+      correctChords: ['im7', 'IV7', 'ivm7', 'V7'],
+    },
+    {
+      src: require('./assets/chords/funk1765.mp3'),
+      correctChords: ['im7', 'VIIM7', 'VIM7', 'V7'],
+    },
+    {
+      src: require('./assets/chords/funk1765alt.mp3'),
+      correctChords: ['im7', 'VIIM7', 'VIM7', 'V7alt'],
+    },
+    {
+      src: require('./assets/chords/funk116dim7.mp3'),
+      correctChords: ['im7', 'im7', 'viø7', 'VII7'],
+    },
+    {
+      src: require('./assets/chords/funk16dim65alt.mp3'),
+      correctChords: ['im7', 'viø7', 'VIM7', 'V7alt'],
+    },
+    {
+      src: require('./assets/chords/funk16dim65.mp3'),
+      correctChords: ['im7', 'viø7', 'VIM7', 'V7'],
+    },
+    {
+      src: require('./assets/chords/funk16dim63.mp3'),
+      correctChords: ['im7', 'viø7', 'VIM7', 'IIIM7'],
+    },
+    {
+      src: require('./assets/chords/funk1b21b2.mp3'),
+      correctChords: ['im7', 'bIIM7', 'im7', 'bIIM7'],
+    },
+    {
+      src: require('./assets/chords/funk16b21.mp3'),
+      correctChords: ['im7', 'VIM7', 'bIIM7', 'im7'],
+    },
+    {
+      src: require('./assets/chords/funk1b21b2.mp3'),
+      correctChords: ['im7', 'bIIM7', 'im7', 'bIIM7'],
+    },
+    {
+      src: require('./assets/chords/funk1645.mp3'),
+      correctChords: ['im7', 'VIM7', 'ivm7', 'V7'],
+    },
+    {
+      src: require('./assets/chords/funk165alt6dim.mp3'),
+      correctChords: ['im7', 'VIM7', 'V7alt', 'viø7'],
+    },
+    {
+      src: require('./assets/chords/funk1566dim.mp3'),
+      correctChords: ['im7', 'V7', 'VIM7', 'viø7'],
+    },
+    {
+      src: require('./assets/chords/funk1645alt.mp3'),
+      correctChords: ['im7', 'VIM7', 'ivm7', 'V7alt'],
     },
   ],
   'jazz': [
